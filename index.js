a7b22f13bbc66e743addbc41646dff3018f7ca06
Kushagra Tiwari <kushagratiwari@Kushagras-MacBook-Air.local>
2024-06-05 22:27:45 +0530

    commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require("dotenv").config()
 
 
 const corsOption = {
-    //origin: 'http://localhost:3000',
+    origin: ['https://chat-32gb.onrender.com'],
     credentials: true
 };
 app.use(cors(corsOption));
