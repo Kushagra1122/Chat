308e59661898582a5ef685f42d7f306d30c5b4b4
Kushagra Tiwari <kushagratiwari@Kushagras-MacBook-Air.local>
2024-06-05 22:46:10 +0530

    commit

--- socket/socket.js
+++ socket/socket.js
@@ -7,8 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['https://chat-32gb.onrender.com'],
-        methods: ['GET', 'POST'],
+       
     },
 });
 const userSocketMap = {};
